92bffbb04435f468ef622befbea5688b7890edb1
Rahul Kumar <57553623+RAHUL81290@users.noreply.github.com>
2022-05-08 23:25:03 +0530

    version2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inputText=document.querySelector(".textinput")
-document.querySelector("i").addEventListener('click',function(){
+document.querySelector("i").addEventListener("click",function(event){
+  if(event)
     fetch("https://api.openweathermap.org/data/2.5/weather?appid=8f308be14ad56d744ced1724ec39b82d&units=metric&q="+inputText.value)
         .then((response) => {
           return response.json();
@@ -18,4 +19,26 @@ document.querySelector("i").addEventListener('click',function(){
         })
         
     });
+    document.addEventListener('keypress',function(event){
+     if(event.key=='Enter'){
+      event.preventDefault();
+       fetch("https://api.openweathermap.org/data/2.5/weather?appid=8f308be14ad56d744ced1724ec39b82d&units=metric&q="+inputText.value)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            var mycity=data.name
+            var Tempdata=data.main.temp
+            var myDisc=data.weather[0].description
+            var iconid=data.weather[0].icon
+            var iconurl="http://openweathermap.org/img/wn/"+iconid+"@2x.png"
+            document.querySelector(".temptext").innerHTML=Tempdata+" °C"
+            document.querySelector(".citytext").innerHTML=mycity
+            document.querySelector(".disctext").innerHTML=myDisc
+            document.querySelector(".image").innerHTML=`<img src="${iconurl}" style= 'height:100px; width:100px;'/>`
+            document.querySelector(".container").classList.add("containeractive")
+          })
+        }
+          
+       });
     
