2a4022d6dc167df89c71d9064ce21a25cd57d7c1
Rahul Kumar <57553623+RAHUL81290@users.noreply.github.com>
2022-06-29 22:23:02 +0530

    Add files via upload
    version 2

--- index.js
+++ index.js
@@ -20,24 +20,6 @@ function result(){
 }
 document.querySelector("i").addEventListener("click",function(){
   result();
-    /*fetch("https://api.openweathermap.org/data/2.5/weather?appid=8f308be14ad56d744ced1724ec39b82d&units=metric&q="+inputText.value)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          var mycity=data.name
-          var Tempdata=data.main.temp
-          var myDisc=data.weather[0].description
-          var iconid=data.weather[0].icon
-          var iconurl="http://openweathermap.org/img/wn/"+iconid+"@2x.png"
-          document.querySelector(".temptext").innerHTML=Tempdata+" °C"
-          document.querySelector(".citytext").innerHTML=mycity
-          document.querySelector(".disctext").innerHTML=myDisc
-          document.querySelector(".image").innerHTML=`<img src="${iconurl}" style= 'height:100px; width:100px;'/>`
-          document.querySelector(".container").classList.add("containeractive")
-        })*/
-        
-        
     });
     document.addEventListener('keypress',function(event){
      if(event.key=='Enter')
